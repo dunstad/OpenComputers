a92029605fb75d8186150612d1808c73f40990d2
dunstad <anthony00.blount00@gmail.com>
2015-09-23 00:13:46 -0500

    update code for receivers

--- inventory/receiver/update.lua
+++ inventory/receiver/update.lua
@@ -1,4 +1,4 @@
 os.execute("cd /usr/inventory")
 os.execute("wget https://raw.githubusercontent.com/dunstad/OpenComputers/master/inventory/inventory.lua")
-os.execute("wget ")
+os.execute("wget https://raw.githubusercontent.com/dunstad/OpenComputers/master/inventory/receiver/receiveRequest.lua")
 os.execute("receiveRequest.lua")
